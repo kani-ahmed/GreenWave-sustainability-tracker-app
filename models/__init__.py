f1e69e6bd163f3206951f94fc01bdf4d731dca1e
kenny-ahmedd <kenny.ahmedd@gmail.com>
2024-03-20 00:02:24 -0400

    Add updated .gitignore

--- models/__init__.py
+++
@@ -1,5 +0,0 @@
-# __init__.py
-
-from .challenge_models import EnvironmentalImpact, Challenge, PersonalChallengeParticipant, Badge, CommunityChallenge, CommunityChallengeParticipant
-from .community_models import Post, Like, Comment, Friendship
-from .user_models import User, UserAction, Notification, UserPreference
