0c11e23d982518ba895f90932daf63e8f2be7c5f
Preston DeLeo <121273342+prestondeleo@users.noreply.github.com>
2024-04-19 21:40:18 -0400

    added get_users api endpoint

--- views/social_views.py
+++ views/social_views.py
@@ -151,6 +151,18 @@ def register_social_routes(app):
         } for post in posts]
 
         return jsonify(post_list), 200
+    
+        @app.route('/get_users', methods=['GET'])
+        def get_all_users():
+            users = User.query.all()
+            user_list = [{
+                'user_id': user.id,
+                'username': user.username,
+                'email': user.email,
+                # Add any other user fields you want to include
+            } for user in users]
+
+            return jsonify(user_list), 200
 
 
 
